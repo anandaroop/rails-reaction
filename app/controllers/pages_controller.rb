83e1ac3f060efb2ecf91b610e54b31de03380830
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-04-17 17:17:39 -0400

    Add some static pages for React components

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def hello_world
+  end
+
+  def reaction_test
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  
+  get 'pages/hello_world', as: :hello_world
+  get 'pages/reaction_test', as: :reaction_test
 end
